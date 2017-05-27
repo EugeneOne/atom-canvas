9ee1812f54accfa96b85d6b68d18543a047d7e99
wanyuqing <wyq_life@yeah.net>
2017-05-27 14:46:01 +0800

    new

---
+++ src/components/Hello.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <h2>Essential Links</h2>
+    <myCanvas :dotsNum="dotsNum" :isColor="false"></myCanvas>
+  </div>
+</template>
+
+<script>
+import myCanvas from './canvas'
+export default {
+  name: 'hello',
+  components: {
+    myCanvas
+  },
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App',
+      dotsNum: 60,
+      roundColor: "red",
+      lineColor: ""
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
